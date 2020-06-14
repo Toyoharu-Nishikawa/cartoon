904ee3ce9ef024b7affe19e76c569ee5bf65d171
toyoharu nishikawa <toyohal@email.com>
2020-06-14 21:29:56 +0900

    add particle demo

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,11 @@
-import {initialize} from "./gui/index.js"
+import {basic, duct} from "./gui/index.js"
 import {login} from "/necoengine/scripts/necoengine/login/index.js"
+
 
 "use ustrict"
 login.setLoginButton()
 login.visit()
 
-initialize()
+//basic()
+duct()
 
